e898e9c49d7ae210758c4dcdfb592078667403be
herysoecahyono <herysoecahyono@users.noreply.github.com>
2025-01-26 13:13:06 +0700

    Update kodeadsterrabanner.js

--- kodeadsterrabanner.js
+++ kodeadsterrabanner.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '2ea86a1d41338bf9c8394ecfddaa91dd',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-prosecutorremarkablegodforsaken.com/2ea86a1d41338bf9c8394ecfddaa91dd/invoke.js
+src='//prosecutorremarkablegodforsaken.com/e2/a1/8e/e2a18e00f100d838fa52d8b36f241eab.js'
