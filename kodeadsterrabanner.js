6e23f9b362697275a3e410ad1f0dd7032a84f00a
herysoecahyono <herysoecahyono@users.noreply.github.com>
2025-01-26 10:35:31 +0700

    Create kodeadsterrabanner.js

---
+++ kodeadsterrabanner.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '2ea86a1d41338bf9c8394ecfddaa91dd',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//prosecutorremarkablegodforsaken.com/2ea86a1d41338bf9c8394ecfddaa91dd/invoke.js"></script>
