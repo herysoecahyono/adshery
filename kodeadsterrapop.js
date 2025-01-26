98d6fbe7eaa8b5b6a429e205d6fba0c159aa1705
herysoecahyono <herysoecahyono@users.noreply.github.com>
2025-01-26 11:45:16 +0700

    Update kodeadsterrapop.js

--- kodeadsterrapop.js
+++ kodeadsterrapop.js
@@ -1,1 +1,1 @@
-src='//prosecutorremarkablegodforsaken.com/e2/a1/8e/e2a18e00f100d838fa52d8b36f241eab.js'>
+<script type='text/javascript' src='//prosecutorremarkablegodforsaken.com/e2/a1/8e/e2a18e00f100d838fa52d8b36f241eab.js'></script> 
